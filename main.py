b45d1d18e9bb48261468650b21c24dcc0ddad533
danny <40965142+dbdoan@users.noreply.github.com>
2025-04-24 00:40:07 -0500

    csv parse function completed
    - function to read and store csv data done

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+import csv
 import os
 import subprocess
 
@@ -5,15 +7,42 @@ os.system(
     "cls" if os.name == "nt" else "clear"
 )
 
-csv_path = "test_input\\input.csv"
-ppttc_path = "test_input\\template.pptx"
+input_csv_path = "test_input\\input.csv"
+input_template_path = "test_input\\template.pptx"
+ppttc_output_path = "test\\input\\ppttc_output.ppttc"
 pptx_output_path = "test_input\\output.pptx"
 
-# 1. Take a CSV input and convert it to JSON structure
+def read_csv(csv_file):
+    data = {}
+    with open(csv_file, 'r') as file:
+        reader = csv.DictReader(file)
+        
+        for row in reader:
+            year = row.pop('Year')
+            data[year] = {series_num: int(count)
+                          for series_num, count in row.items()}
+            # print(row.items())
+    return data
 
+data = read_csv(input_csv_path)
+
+# # 1. Take a CSV input and convert it to JSON structure
+def csv_to_json(data, ppttc_template):
+    data = defaultdict(lambda: defaultdict(int), data)
+
+    years = sorted(data.keys())
+
+    print(years)
+    
+    
+csv_to_json(data, input_template_path)
+
+
+
+# csv_to_json(test_csv, input_template_path)
 # 2. Take JSON and convert to PPTTC
 
-# 3. Use CLI to call .exe and convert PPTTC to PPTX
+# 3. Use CLI to call .exe and utilize PPTTC data + PPTX_template to PPTX
 
 # def run_thinkcell_cli(ppttc_file, output_pptx):
 #     command = [
