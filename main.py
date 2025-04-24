3e484477d4ba99eccdc765424781332882b6db04
danny <40965142+dbdoan@users.noreply.github.com>
2025-04-24 02:29:51 -0500

    Completed core functionality
    - Generates PPTX based on CSV input and template

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import csv
+import json
 import os
 import subprocess
 
@@ -9,7 +10,7 @@ os.system(
 
 input_csv_path = "test_input\\input.csv"
 input_template_path = "test_input\\template.pptx"
-ppttc_output_path = "test\\input\\ppttc_output.ppttc"
+ppttc_output_path = "ppttc_output.ppttc"
 pptx_output_path = "test_input\\output.pptx"
 
 def read_csv(csv_file):
@@ -24,39 +25,62 @@ def read_csv(csv_file):
             # print(row.items())
     return data
 
-data = read_csv(input_csv_path)
-
-# # 1. Take a CSV input and convert it to JSON structure
+# # 1. Take a CSV input and convert it to JSON structure for PPTTC
 def csv_to_json(data, ppttc_template):
     data = defaultdict(lambda: defaultdict(int), data)
 
     years = sorted(data.keys())
+    series = sorted(set(type for year in data for type in data[year]))
 
-    print(years)
-    
-    
-csv_to_json(data, input_template_path)
+    json_structure = {
+        "template": ppttc_template,
+        "data": [
+            {
+                "name": "Title",
+                "table": [[{"string": "Slide #2"}]]
+            },
+            {
+                "name": "Chart1",
+                "table":
+                [
+                    # Header w/ years
+                    [None] + 
+                    [{"string": str(year)} for year in years],
 
+                    # Empty row after header
+                    []
+                ] + [
+                    [{"string": s}] + [{"number": data[year][s]}
+                                                      for year in years]
+                    for s in series
+                ]
+            }
+        ]
+    }
+    return json.dumps([json_structure], indent=4)
+# print(csv_to_json(data, input_template_path))
 
+def run_thinkcell_cli(ppttc_file, output_pptx):
+    command = [
+        "C:\\Program Files (x86)\\think-cell\\ppttc.exe", ppttc_file, '-o', output_pptx
+    ]
 
-# csv_to_json(test_csv, input_template_path)
+    try:
+        result = subprocess.run(
+            command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+    except subprocess.CalledProcessError as e:
+        print("Error:", e)
+        print("Standard Output:", e.stdout)
+        print("Standard Error:", e.stderr)
+
+data = read_csv(input_csv_path)
+
 # 2. Take JSON and convert to PPTTC
+json_data_for_ppttc = csv_to_json(data, input_template_path)
+with open(ppttc_output_path, 'w') as file:
+    file.write(json_data_for_ppttc)
 
-# 3. Use CLI to call .exe and utilize PPTTC data + PPTX_template to PPTX
-
-# def run_thinkcell_cli(ppttc_file, output_pptx):
-#     command = [
-#         "C:\\Program Files (x86)\\think-cell\\ppttc.exe", ppttc_file, '-o', output_pptx]
-
-#     # Execute the command using subprocess.
-#     try:
-#         result = subprocess.run(
-#             command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-#         print("think-cell processing successful.")
-#     except subprocess.CalledProcessError as e:
-#         print("Error:", e)
-#         print("Standard Output:", e.stdout)
-#         print("Standard Error:", e.stderr)
-
-# if __name__ == "__main__":
-#     print("test")
+if __name__ == "__main__":
+    # 3. Use CLI to call .exe and utilize PPTTC data + PPTX_template to PPTX
+    run_thinkcell_cli(ppttc_output_path, pptx_output_path)
