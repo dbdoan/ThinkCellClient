0537fe6b3ae7d15c28f2e07a95856f3757d2c764
danny <40965142+dbdoan@users.noreply.github.com>
2025-04-25 23:40:24 -0500

    Contacts Implemented
    - Added animated social icons
    - Contact links

--- Thinkcell-Panel/src/App.jsx
+++ Thinkcell-Panel/src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import IntroComponent from './components/TypingEffect.jsx'
-import { handleKeyFileChange, handleTemplateFileChange } from './components/fileValidator.js';
 
-import './App.scss';
+import './App.css'
+import { useState } from 'react'
+// import linkedInLogo from './assets/linkedin.svg'
+// import githubLogo from './assets/github.svg'
+import IntroComponent from './components/TypingEffect.jsx'
+import { handleKeyFileChange, handleTemplateFileChange } from './components/fileValidator.js'
+
+import LinkedInIcon from './components/linkedinLogo.jsx';
+import GithubIcon from './components/githubLogo.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
-    return (
-      <>
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Files successfully submitted");
+  }
+
+
+  return (
+
+    <>
       <div className="top-text">
         <div className="typing-effect-intro">
           <IntroComponent />
@@ -23,14 +34,27 @@ function App() {
       </div>
 
       <div className="card">
-        <form id="upload-form">
+        <form id="upload-form" onSubmit={handleSubmit}>
           <p className="instructions">To generate a Thinkcell PPTX, 2 files are needed:</p>
-          <p>1. PPTTC (key) OR a properly formatted CSV</p>
+          <p>1. PPTTC (key) or a properly formatted CSV</p>
           <input className="choose-file" type="file" name="jsonFile" accept=".ppttc, .csv" onChange={ handleKeyFileChange }/>
           <p>2. PPTTC Template</p>
           <input className="choose-file" type="file" name="ppttcFile" accept=".ppttc" onChange={ handleTemplateFileChange } />
           <button className="submit-btn" type="submit">Upload</button>
         </form>
+        <div className="output">
+
+        </div>
+      </div>
+      
+      <div className="footer">
+        <a className="social-logo" href="https://www.linkedin.com/in/dbdoan/" target="_blank" rel="noreferrer" alt="button to LinkedIn profile">
+          <LinkedInIcon />
+        </a>
+
+        <a className="social-logo" href="https://github.com/dbdoan/ThinkCellClient" target="_blank" rel="noreferrer" alt="button to Github source code">
+          <GithubIcon />
+        </a>
       </div>
     </>
   )
